df2a32132dd1848b8263f7eabc609c50a5080ba8
nathanielpierson <nathanielepierson@gmail.com>
2025-01-30 13:17:40 -0500

    fixes

---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  <footer>
+    <p>Copyright 20XX</p>
+  </footer>
+}
--- src/PostIndex.jsx
+++ src/PostIndex.jsx
@@ -1,7 +1,16 @@
-function PostIndex() { }
-posts = { posts };
-{
-  <div id="posts-index">
-    {props.posts.map}
-  </div>;
+function PostIndex({ posts }) {
+  return (
+    <div id="recipes-index">
+      <h1>All recipes</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>body: {post.body}</p>
+          <p><img src={post.image_url} /></p>
+          <button>More info</button>
+        </div>
+      ))}
+
+    </div>
+  );
 }
